affab3805defa3a19db5fb11708e6f42777ed4f0
Nanna Engell <nanna_engell@hotmail.com>
2016-10-09 15:20:58 +0200

    Made the test in linear_kernel a bit faster

--- Handin2/linear_kernel.py
+++ Handin2/linear_kernel.py
@@ -3,7 +3,7 @@ from sklearn import svm
 from Handin2.util import get_data
 
 images, labels = get_data('Data/auTrain.npz')
-
+images_test, labels_test = get_data('Data/auTest.npz')
 
 for i in range(10):
     print('C:', 0.1*i)
@@ -14,6 +14,5 @@ for i in range(10):
     in_sample_accuracy = (clf.predict(images)==labels).mean()
     print('In sample Accuracy {:.2%}, In Sample Error {:.4f} '.format(in_sample_accuracy,1-in_sample_accuracy))
 
-    images_test, labels_test = get_data('Data/auTest.npz')
     out_of_sample_accuracy = (clf.predict(images_test)==labels_test).mean()
     print('Out of sample Accuracy {:.2%}, In Sample Error {:.4f}'.format(out_of_sample_accuracy,1-out_of_sample_accuracy))
