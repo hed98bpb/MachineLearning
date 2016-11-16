7362c1fde6dfd7269e43241daae41b31d14a6bb2
Nanna Engell <nanna_engell@hotmail.com>
2016-11-16 18:06:47 +0100

    Viterbi and viterbi_log virker 100procent nu

--- CodeExercise3/hmm_vit.py
+++ CodeExercise3/hmm_vit.py
@@ -18,29 +18,30 @@ omega = [[0 for col in range(N)] for row in range(K)]
 for i in range(K):
     omega[i][0] = pi[i] * phi[i][obs[X[0]]]
 
-# Computing omega[z_n] for n>1:
+# Computing omega[z_n] for n>0:
 for n in range(1, N):
     for k in range(K):
-        if phi[k][obs[X[n]]] != 0:
-            for j in range(len(obs)):
-                if A[k][j] != 0:
-                    omega[k][n] = max(omega[k][n], phi[k][obs[X[n]]] * omega[j][n-1] * A[k][j])
+        max_n = 0
+        for j in range(K):
+            possible_max = omega[j][n-1] * A[j][k]
+            if possible_max > max_n:
+                max_n = possible_max
+        omega[k][n] = phi[k][obs[X[n]]] * max_n
 
 # Backtracking - finding Z*:
 omega = np.array(omega) # TODO: we should do the computations above with np.arrays instead
 max_z_N_index = np.argmax(omega[:, N-1])
 
+print('P(X,Z) (likelihood): ', omega[max_z_N_index][N-1])
 Z_star = inv_states[max_z_N_index] # The last state in the string Z_star.
 
 for n in reversed(range(N-1)):
     column_n = omega[:, n]
     for k in range(K):
-        column_n[k] = phi[k][obs[X[n + 1]]] * omega[k, n] * A[int(states[(Z_star[0])])][k]
+        column_n[k] = phi[states[Z_star[0]]][obs[X[n + 1]]] * omega[k , n] * A[k][int(states[(Z_star[0])])]
     z_n_index = np.argmax(column_n)
 
     Z_star = inv_states[z_n_index] + Z_star
 
 # printing the overall most likely explanation of X:
 print(Z_star)
-
-
